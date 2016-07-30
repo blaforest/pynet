576805b093ee1ffb364c04c77cd91240ffb77b8a
Briand LaForest <blaforest@oit.umass.edu>
2016-07-30 16:29:34 -0700

    Adding test_createYAML_JSON

---
+++ test_createYAML_JSON.py
@@ -0,0 +1,19 @@
+import yaml
+import json
+
+my_list = range(5)
+my_list.append('Python Programming')
+my_list.append('Is Fun')
+my_list.append({})
+my_list[-1]['IP_ADDR'] = '10.10.10.239'
+my_list[-1]['HOSTNAME'] = 'testbox'
+my_list[-1]['DOMAIN_NAME'] = 'someplace.net'
+
+with open("Lesson1Number6_first_yaml.yml", "w") as filehandle1:
+	filehandle1.write(yaml.dump(my_list, default_flow_style=False))
+	filehandle1.close()
+
+with open("Lesson1Number6_first_json.json", "w") as filehandle2:
+	json.dump(my_list, filehandle2)
+	filehandle2.close()
+
