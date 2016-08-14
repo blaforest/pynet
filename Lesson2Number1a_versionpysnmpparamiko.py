3be1e601ab443599710d23d636cbc08f70bff69d
Briand LaForest <blaforest@oit.umass.edu>
2016-08-14 07:38:09 -0700

    Adding Lesson2Number1a_versionpysnmpparamiko.py

---
+++ Lesson2Number1a_versionpysnmpparamiko.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import pysnmp
+import paramiko
+
+print '\n\nVersion of pysnmp installed:'
+print '\n\t', pysnmp.__version__
+
+print '\n\nVersion of paramiko installed:'
+print '\n\t', paramiko.__version__
+
+print "\n\n"
